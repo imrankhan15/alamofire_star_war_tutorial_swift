1a6cca19475a4780bee176716471994388c7548c
imrankhan15 <buetcse110@gmail.com>
2018-12-31 10:54:03 +0100

    Initial Commit

---
+++ star wars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  star wars
+//
+//  Created by Muhammad Faisal Imran Khan on 31/12/18.
+//  Copyright © 2018 MI Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
